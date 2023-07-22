cae5962d211420f49314d252f29a40016f9497cd
Egor <egeberleyn@gmail.com>
2023-07-23 00:42:53 +0300

    Add application tests for cats

---
+++ KittyStore.Server/tests/UnitTests/KittyStore.Application.UnitTests/TestUtils/Cats/Extensions/CatExtensions.Validations.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using KittyStore.Application.Cats.Commands.CreateCat;
+using KittyStore.Domain.CatAggregate;
+
+namespace KittyStore.Application.UnitTests.TestUtils.Cats.Extensions;
+
+public static partial class CatExtensions
+{
+    public static void ValidateCreatedFrom(this Cat cat, CreateCatCommand command)
+    {
+        cat.Id.Should().NotBeEmpty();
+        cat.Name.Should().Be(command.Name);
+        cat.Age.Should().Be(command.Age);
+        cat.Gender.ToString().Should().Be(command.Gender);
+        cat.Breed.Should().Be(command.Breed);
+        cat.Color.Should().Be(command.Color);
+        cat.Price.Should().Be(command.Price);
+    }
+}
---
+++ KittyStore.Server/tests/UnitTests/KittyStore.Application.UnitTests/TestUtils/Constants/Constants.Cat.cs
@@ -0,0 +1,14 @@
+﻿namespace KittyStore.Application.UnitTests.TestUtils.Constants;
+
+public static partial class Constants
+{
+    public static class Cat
+    {
+        public const string Name = "Bars";
+        public const string Color = "blue";
+        public const int Age = 12;
+        public const string Breed = "kun'lun";
+        public const string Gender = "Male";
+        public const decimal Price = 50;
+    }
+}
